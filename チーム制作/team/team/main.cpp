209d08493045048fbcf635307947e19e41297451
kirimnoji <65146180+kirimnoji@users.noreply.github.com>
2020-05-12 08:55:46 +0900

    main.cppの追加 main.hの追加 WinMain関数の追加 ｹﾞｰﾑﾙｰﾌﾟの枠だけ追加

---
+++ チーム制作/team/team/main.cpp
@@ -0,0 +1,136 @@
+#include "DxLib.h"
+#include "main.h"
+
+// WinMainä÷êî
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	/*if (!SysInit())
+	{
+		return 0;
+	}*/
+
+	// èâä˙âªä÷êîåƒÇ—èoÇµ
+	/*SysInit();*/
+
+	// πﬁ∞—Ÿ∞Ãﬂ
+	while (ProcessMessage() == 0 &&
+		CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		/*KeyCheck();
+		gameCounter++;*/
+
+		// º∞›ëJà⁄ópΩ≤Ø¡ï∂
+		/*switch (scnID)
+		{
+		case SCN_ID_INIT:
+			InitScene();
+			scnID = SCN_ID_TITLE;
+			break;
+		case SCN_ID_TITLE:
+			if (fadeIn)
+			{
+				if (!FadeInScreen(5))
+				{
+
+				}
+			}
+			if (fadeOut)
+			{
+				if (!FadeOutScreen(5))
+				{
+					fadeIn = true;
+					scnID = SCN_ID_SELECT;
+				}
+			}
+
+			TitleInit();
+			break;
+		case SCN_ID_SELECT:
+			if (fadeIn)
+			{
+				if (!FadeInScreen(5))
+				{
+
+				}
+			}
+			if (fadeOut)
+			{
+				if (!FadeOutScreen(5))
+				{
+					fadeIn = true;
+					scnID = SCN_ID_GAME;
+				}
+			}
+			SelectInit();
+			break;
+		case SCN_ID_GAME:
+			if (fadeIn)
+			{
+				if (!FadeInScreen(5))
+				{
+
+				}
+			}
+			if (fadeOut)
+			{
+				if (!FadeOutScreen(5))
+				{
+					fadeIn = true;
+					scnID = SCN_ID_RESULT;
+				}
+			}
+			GameInit();
+			break;
+		case SCN_ID_RESULT:
+			if (fadeIn)
+			{
+				if (!FadeInScreen(5))
+				{
+
+				}
+			}
+			if (fadeOut)
+			{
+				if (!FadeOutScreen(5))
+				{
+					fadeIn = true;
+					if (titleFlag)
+					{
+						scnID = SCN_ID_TITLE;
+					}
+					if (selectFlag)
+					{
+						scnID = SCN_ID_SELECT;
+					}
+					if (gameOverFlag)
+					{
+						scnID = SCN_ID_GAMEOVER;
+					}
+				}
+			}
+			ResultInit();
+			break;
+		case SCN_ID_GAMEOVER:
+			if (fadeIn)
+			{
+				if (!FadeInScreen(5))
+				{
+
+				}
+			}
+			if (fadeOut)
+			{
+				if (!FadeOutScreen(5))
+				{
+					fadeIn = true;
+					scnID = SCN_ID_TITLE;
+				}
+			}
+			GameOverInit();
+			break;
+		}*/
+	}
+
+	DxLib_End();
+	return 0;
+}

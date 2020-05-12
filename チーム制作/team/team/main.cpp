29b76c0b7eee4a4150563162e2a6291cfc74b8a4
taishikamizaki <k-taishi@outlook.jp>
2020-05-12 13:15:32 +0900

    playerの変数を追加

--- チーム制作/team/team/main.cpp
+++ チーム制作/team/team/main.cpp
@@ -1,6 +1,7 @@
 #include "DxLib.h"
 #include "main.h"
 
+int Player;
 // WinMainä÷êî
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
@@ -129,6 +130,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			GameOverInit();
 			break;
 		}*/
+
 	}
 
 	DxLib_End();

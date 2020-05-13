c778040d7b7887d74b8802cb028859b7ea6a044d
Takayoshi1125 <takayoshi176@gmail.com>
2020-05-13 12:25:08 +0900

    player追加

---
+++ チーム制作/team/team/player.h
@@ -0,0 +1,27 @@
+#pragma once
+
+//定数の定義
+
+//構造体
+
+struct CHARACTER {
+
+	bool runFlag;		//キャラクタの状態（走っているか？）
+	bool jumpFlag;		//キャラクタの状態（ジャンプしているか？）
+	bool shotFlag;		//キャラクタの状態（弾撃っているか？）
+	bool damageFlag;	//キャラクタの状態（ダメージ受けているか？）
+	bool visible;		//表示状態
+	int moveSpeed;		//キャラクタの移動量
+	int life;		//キャラクタの体力
+	int lifeMax;		//キャラクタの体力最大
+	int animCnt;		//キャラクタのアニメーション用カウンタ
+	int imgLockCnt;		//キャラクタのイメージ固定用カウンタ
+	int type;			//何らかの属性を格納できる。
+
+};
+
+//プロトタイプ宣言
+void	PlayerSystemInit(void);
+void	PlayerGameINit(void);
+void	PlayerControl(void);
+void	PlayerGameDrow(void);

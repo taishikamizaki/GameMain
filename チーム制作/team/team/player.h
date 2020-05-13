76a51bf4d5c80f96bbacb6f4deae5a488fecc670
Takayoshi1125 <takayoshi176@gmail.com>
2020-05-13 16:23:31 +0900

    更新

--- チーム制作/team/team/player.h
+++ チーム制作/team/team/player.h
@@ -2,26 +2,49 @@
 
 //定数の定義
 
+//列挙型
+
+enum DIR 
+{
+	DIR_UP,
+	DIR_RIGHT,
+	DIR_DOWN,
+	DIR_LEFT,
+	DIR_MAX
+};
+
+enum CHAR_ID
+{
+	CHAR_ID_KISI
+	,CHAR_ID_MDOU
+	,CHAR_ID_BTOU
+	,CHAR_ID_4
+	,CHAR_ID_MAX
+
+};
+
 //構造体
 
-struct CHARACTER {
 
+struct CHARACTER 
+{
+	DIR moveDir;		//向いている方向
+	Pos pos;			//キャラクタの位置（中心）
+	Pos size;			//キャラクタ画像のサイズ
+	Pos sizeOffset;		//キャラクタ中央からの左上位置
 	bool runFlag;		//キャラクタの状態（走っているか？）
 	bool jumpFlag;		//キャラクタの状態（ジャンプしているか？）
 	bool shotFlag;		//キャラクタの状態（弾撃っているか？）
 	bool damageFlag;	//キャラクタの状態（ダメージ受けているか？）
-	bool visible;		//表示状態
 	int moveSpeed;		//キャラクタの移動量
-	int life;		//キャラクタの体力
+	int life;			//キャラクタの体力
 	int lifeMax;		//キャラクタの体力最大
 	int animCnt;		//キャラクタのアニメーション用カウンタ
-	int imgLockCnt;		//キャラクタのイメージ固定用カウンタ
-	int type;			//何らかの属性を格納できる。
 
 };
 
 //プロトタイプ宣言
 void	PlayerSystemInit(void);
-void	PlayerGameINit(void);
+void	PlayerGameInit(void);
 void	PlayerControl(void);
-void	PlayerGameDrow(void);
+void	PlayerGameDraw(void);

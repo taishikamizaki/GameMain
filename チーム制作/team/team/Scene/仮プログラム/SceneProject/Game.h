d13692a75742801afbeb8c67ad98ddeb6a90a404
kirimnoji <kirimnoji@gmail.com>
2020-08-31 10:02:42 +0900

    更新

--- チーム制作/team/team/Scene/仮プログラム/SceneProject/Game.h
+++ チーム制作/team/team/Scene/仮プログラム/SceneProject/Game.h
@@ -1,25 +1,28 @@
 #pragma once
+
+// ゲームクラス
 class Game
 {
 public:
+
+	// フラグゲッター
 	int GetFlag()
 	{
 		return _gameF;
 	}
 
+	int GameCtl();		// 操作系
+	int GameDraw();		// 描画
 
-	int GameCtl();
-	int GameDraw();
-
-	Game();
-	~Game();
+	Game();		// コンストラクタ
+	~Game();	// デストラクタ
 
 private:
 
-	int Init();
+	int Init();		// 初期化
 
-	int select;
-	int game;
+	int select;		// 仮置き画像用
+	int game;		// 仮置き画像用
 
 	bool gameF;		// 切り替え用
 	bool _gameF;	// シーン遷移用
